1d50da57a9051c401d34f70d8db7d7776c4e58da
architecturequeen <olawunmitaylor@gmail.com>
2018-12-09 22:27:27 +0000

    feat: create skeleton app with redux and routing support

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import rootReducer from './reducers';
+
+import { BrowserRouter as Router, Link, Route } from 'react-router-dom';
 import './App.css';
+
+const store = createStore(rootReducer);
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Provider store={store}>
+        <Router>
+          <div className="App">
+            Login
+            <Link to="/">Home</Link> |<Link to="/create">Create New Poll</Link>{' '}
+            |<Link to="/board">Leader Board</Link> |<button>Logout</button>
+            <span>Logged in As User X</span>
+            <Route path="/" exact component={() => <div>Home</div>} />
+            <Route
+              path="/create/"
+              component={() => <div>Create New Poll</div>}
+            />
+            <Route path="/board/" component={() => <div>Leader Board</div>} />
+          </div>
+        </Router>
+      </Provider>
     );
   }
 }
