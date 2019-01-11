5a7c941143f56f3f05e373f7cacd60cd451b537f
architecturequeen <olawunmitaylor@gmail.com>
2019-01-11 23:41:09 +0000

    feat: update root of app

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import rootReducer from './reducers';
 
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
-import LoginList from './LoginList';
+import LoginContainer from './LoginContainer';
 
 const store = createStore(rootReducer);
 
@@ -15,7 +15,7 @@ class App extends Component {
       <Provider store={store}>
         <Router>
           <div className="App">
-            <LoginList />
+            <Route component={LoginContainer} />
           </div>
         </Router>
       </Provider>
