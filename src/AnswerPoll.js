802e70c98d1cad3b1382263157d602a8500f9ebd
architecturequeen <olawunmitaylor@gmail.com>
2019-01-11 23:38:49 +0000

    feat: create answer poll page

---
+++ src/AnswerPoll.js
@@ -0,0 +1,63 @@
+import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
+
+class AnswerPoll extends Component {
+  state = {
+    value: 'optionOne'
+  };
+
+  handleSubmit = () => {
+    console.log('handleSubmit');
+    this.props.submitAnswer(
+      this.props.user,
+      this.props.questionID,
+      this.state.value
+    );
+    this.props.history.push(`/result/${this.props.questionID}`);
+  };
+
+  handleChange = event => {
+    this.setState({ value: event.target.value });
+  };
+  render() {
+    const { author, optionOne, optionTwo, avatar, users } = this.props;
+    return (
+      <div>
+        <div> {author} asks</div>
+        <div>
+          <div>
+            <img src={avatar} alt="user avatar" />
+          </div>
+          <div>
+            <p>Would you rather...</p>
+            <form>
+              <input
+                type="radio"
+                name="options"
+                value="optionOne"
+                checked={this.state.value === 'optionOne'}
+                onChange={this.handleChange}
+              />
+              <label>{optionOne}</label>
+              <input
+                type="radio"
+                name="options"
+                value="optionTwo"
+                checked={this.state.value === 'optionTwo'}
+                onChange={this.handleChange}
+              />
+              <label>{optionTwo}</label>
+              <input
+                type="button"
+                value="Submit Answer"
+                onClick={this.handleSubmit}
+              />
+            </form>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default withRouter(AnswerPoll);
---
+++ src/AnswerPollContainer.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
+import { connect } from 'react-redux';
+
+import AnswerPoll from './AnswerPoll';
+import { submitVote } from './actions';
+import * as Data from './Data';
+
+class AnswerPollContainer extends Component {
+  state = {
+    author: '',
+    optionOne: '',
+    optionTwo: ''
+  };
+
+  render() {
+    const { loggedInUser, questions, users, submitAnswer } = this.props;
+    const quID = this.props.match.params.id;
+    return (
+      <AnswerPoll
+        author={questions[quID].author}
+        questionID={quID}
+        user={loggedInUser}
+        optionOne={questions[quID].optionOne.text}
+        optionTwo={questions[quID].optionTwo.text}
+        avatar={users[questions[quID].author].avatarURL}
+        users={users}
+        submitAnswer={submitAnswer}
+      />
+    );
+  }
+}
+
+const updateAnswers = (dispatch, uid, quid, answer) => {
+  Data._saveQuestionAnswer({ authedUser: uid, qid: quid, answer }).then(() => {
+    dispatch(submitVote({ user: uid, quid, answer }));
+  });
+};
+
+const mapStateToProps = state => ({
+  loggedInUser: state.loggedInUser,
+  questions: state.questions,
+  users: state.users
+});
+
+const mapDispatchToProps = dispatch => ({
+  submitAnswer: (uid, quid, answer) =>
+    updateAnswers(dispatch, uid, quid, answer)
+});
+
+export default withRouter(
+  connect(
+    mapStateToProps,
+    mapDispatchToProps
+  )(AnswerPollContainer)
+);
